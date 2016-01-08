6a4dcf907e8faec0c51924782948a2c97612ce35
Sean Quinn <seancquinn@users.noreply.github.com>
2016-01-08 10:25:36 -0600

    Update cs_test.py

--- cs_test.py
+++ cs_test.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import division
-import pandas as pd
-import numpy as np
-from sklearn.preprocessing import StandardScaler
-from sklearn.cross_validation import KFold
-from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier as RF
-from sklearn.neighbors import KNeighborsClassifier as KNN
+# import pandas as pd
+# import numpy as np
+# from sklearn.preprocessing import StandardScaler
+# from sklearn.cross_validation import KFold
+# from sklearn.svm import SVC
+# from sklearn.ensemble import RandomForestClassifier as RF
+# from sklearn.neighbors import KNeighborsClassifier as KNN
 import urllib
 import requests
 import os
@@ -46,53 +46,3 @@ script_history = requests.get(urllib.parse.urljoin(endpoint, '/scripts/%d/histor
 
 file_loc = script_history[0]['output'][0]['path']
 print(file_loc)
-
-with tempfile.NamedTemporaryFile() as temp:
-	response = requests.get(file_loc).iter_content(10000)
-	for r in response:
-		temp.write(r)
-	churn_df = pd.read_csv(temp.name)
-
-col_names = churn_df.columns.tolist()
-
-churn_result = churn_df['churn']
-y = np.where(churn_result == 'True.', 1, 0)
-
-to_drop = ['state','area_code','phone','churn']
-churn_feat_space = churn_df.drop(to_drop, axis=1)
-
-yes_no_cols = ["intl_plan", "vmail_plan"]
-churn_feat_space[yes_no_cols] = churn_feat_space[yes_no_cols] == 'yes'
-
-features = churn_feat_space.columns
-
-X = churn_feat_space.as_matrix().astype(np.float)
-
-scaler = StandardScaler()
-X = scaler.fit_transform(X)
-
-print("%d rows and %d features" % X.shape)
-print("targets:", np.unique(y))
-
-
-def run_cv(X, y, clf_class, **kwargs):
-    kf = KFold(len(y), n_folds=5, shuffle=True)
-    y_pred = y.copy()
-    for train_index, test_index in kf:
-        X_train, X_test = X[train_index], X[test_index]
-        y_train = y[train_index]
-        # Initialize a classifier
-        clf = clf_class(**kwargs)
-        clf.fit(X_train,y_train)
-        y_pred[test_index] = clf.predict(X_test)
-    return y_pred
-
-def accuracy(y_true,y_pred):
-    return np.mean(y_true == y_pred)
-
-print("Support vector machines:")
-print("%.3f" % accuracy(y, run_cv(X, y, SVC)))
-print("Random forest:")
-print("%.3f" % accuracy(y, run_cv(X, y, RF)))
-print("K-nearest-neighbors:")
-print("%.3f" % accuracy(y, run_cv(X, y, KNN)))
